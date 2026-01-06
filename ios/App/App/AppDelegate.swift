8e760c6d05feaa63b9fec25fb27fb43a7e5a5069
Melih <melih Kochan>
2026-01-06 20:23:11 +0300

    Refactor iOS AppDelegate user activity handling and update Package.swift dependencies: Modified the continue user activity method to support Universal Links and call the restoration handler appropriately. Removed unnecessary AdMob dependency from Package.swift to streamline project resources.

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -39,11 +39,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return ApplicationDelegateProxy.shared.application(app, open: url, options: options)
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([any UIUserActivityRestoring]?) -> Void) -> Bool {
         // Called when the app was launched with an activity, including Universal Links.
-        // Feel free to add additional processing here, but if you want the App API to support
-        // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        // Capacitor 8 - continue method geçici olarak devre dışı (API değişti)
+        // Universal Links için URL varsa onu kullan
+        if let url = userActivity.webpageURL {
+            return ApplicationDelegateProxy.shared.application(application, open: url, options: [:])
+        }
+        // restorationHandler'ı çağır (boş array)
+        restorationHandler([])
+        return true
     }
 
 }
